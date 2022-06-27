c0e1d369aba749a8c3a1c293a9848e962ff657bc
Ariiel08 <ariel_ortega98@hotmail.com>
2022-06-27 15:57:46 -0400

    Ejercicio C8

---
+++ C8/Assets/Scripts/SphereBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehaviour : MonoBehaviour
+{
+    Vector3 deltaPos = new Vector3();
+    Vector3 currentSpeed = new Vector3();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaPos.y = currentSpeed.y * Time.deltaTime + Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2) / 2;
+        gameObject.transform.Translate(deltaPos);
+        currentSpeed += Physics.gravity * Time.deltaTime;
+    }
+}
